32c672b9f2671276556f5473dd0931e7e83e8717
abhimanyuPathania <abpindia1944@gmail.com>
2014-11-19 01:56:19 +0530

    Add clearRect test to createCanvas

--- scripts/meme.js
+++ scripts/meme.js
@@ -5,26 +5,43 @@ $(document).ready(function(){
 var memeFile = $('#memeFile');
 var meme = $('#meme');
 var memeCanvas = $('#c').get(0);
+var topText = $('#topText');
 
 memeFile.change(function(event){
   event.preventDefault();
+  
   var file = event.target.files[0];
   reader = new FileReader();
+  
   reader.onload = function(event){
     var memeImage = new Image();
     memeImage.src = event.target.result;
     console.log(memeImage.width,memeImage.height);
     createCanvas(memeImage);
   };
+  
   reader.readAsDataURL(file);
 });
 
 function createCanvas(img){
-  //var c = $('<canvas id="c" width="'+ img.width +'" height="'+ img.height +'"></canvas>').get(0); //get the actual canvas from jQuery Object
+
+  var ctx = memeCanvas.getContext("2d");
+  if(memeCanvas.width != 0){
+    //already drawn an image so clear whole canvas                                  
+    ctx.clearRect(0, 0, memeCanvas.width, memeCanvas.height); 
+  }
   memeCanvas.width = img.width;
   memeCanvas.height = img.height;
-  //meme.prepend(c);
-  
-  var ctx = memeCanvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
+}
+
+function generateMemeText(text){
+  var ctx = memeCanvas.getContext("2d");
+      ctx.fillStyle = "white";
+      ctx.font = "36pt Impact";
+      ctx.lineWidth = 1;
+      ctx.textAlign = "center";
+      
+      ctx.fillText(text,(memeCanvas.width)/2, 40);
+      ctx.strokeText(text,(memeCanvas.width)/2, 40);
 }
