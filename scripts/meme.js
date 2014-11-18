f94df32f1367904a6732bd4fe51087510174c4bf
abhimanyuPathania <abpindia1944@gmail.com>
2014-11-19 01:38:54 +0530

    Add Canvas to HTML access it as global var

--- scripts/meme.js
+++ scripts/meme.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
 var memeFile = $('#memeFile');
 var meme = $('#meme');
+var memeCanvas = $('#c').get(0);
 
 memeFile.change(function(event){
   event.preventDefault();
@@ -19,11 +20,11 @@ memeFile.change(function(event){
 });
 
 function createCanvas(img){
-  var c = $('<canvas id="c" width="'+ img.width +'" height="'+ img.height +'"></canvas>').get(0); //get the actual canvas from jQuery Object
-  var canvasWidth = img.width;
-  var canvasheight = img.height;
-  meme.prepend(c);
+  //var c = $('<canvas id="c" width="'+ img.width +'" height="'+ img.height +'"></canvas>').get(0); //get the actual canvas from jQuery Object
+  memeCanvas.width = img.width;
+  memeCanvas.height = img.height;
+  //meme.prepend(c);
   
-  var ctx = c.getContext("2d");
+  var ctx = memeCanvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
 }
