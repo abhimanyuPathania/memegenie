baf0ec05bde2b868bf4c0f8e841190c7c66e9253
abhimanyuPathania <abpindia1944@gmail.com>
2014-11-20 23:44:39 +0530

    Add setContextSettings function Initial code cleaup

--- scripts/meme.js
+++ scripts/meme.js
@@ -1,14 +1,20 @@
 
 // Globals
+
 var memeFile = $('#memeFile');
 var meme = $('#meme');
 var memeCanvas = $('#c').get(0);
-var ctx = memeCanvas.getContext("2d");
 var topText = $('#topText');
 var bottomText = $('#bottomText');
 var error = $('.error');
+
+var ctx = memeCanvas.getContext("2d");
 var memeText = [];
 var memeImage;
+var maxWidth;
+var lineHeight;
+
+
 
 //Event Handlers
 memeFile.change(function(event){
@@ -21,6 +27,7 @@ memeFile.change(function(event){
     var file = event.target.files[0];
     
     // file-type error checking
+    if(!file)return false;
     if(file.type.search("image")== -1){
        return error.show("fast");
       }
@@ -64,10 +71,13 @@ $("#upfile").mouseup(function(){
 function setText(){
   if(!memeImage) return false;
   // Get both, top and bottom, text values and pass to generateMemeText
-  // using global var memeText for future enhancements
+
   memeText[0] = topText.val();
   memeText[1] = bottomText.val();
-  createCanvas(memeImage); // redraw meme everytime you type
+  
+  // redraw meme everytime you type
+  createCanvas(memeImage); 
+        
   generateTopText(memeText[0]);
   generateBottomText(memeText[1]);
 }
@@ -96,15 +106,26 @@ function createThumbnail(img){
   }
   $('#imageThumbnail').append(thumb);
 }
+function setContextSettings(){
+      
+      ctx.fillStyle = "white";
+      ctx.strokeStyle = "black";
+      ctx.font = "40px Impact";
+      ctx.lineWidth = 2;
+      ctx.textAlign = "center";
+      
+      maxWidth = (memeCanvas.width-30);
+      lineHeight = 40;
+}
 
 function createCanvas(img){
   
-  if(!memeImage)return false; //if text is tried to set before loading meme
+  //if text is tried to set before loading meme
+  if(!memeImage)return false; 
   var scaling;
   var w = img.width;
   var h = img.height;
-  //var ctx = memeCanvas.getContext("2d");
-  
+
   if(memeCanvas.width != 0){
     //already drawn an image so clear whole canvas                                  
     ctx.clearRect(0, 0, memeCanvas.width, memeCanvas.height); 
@@ -119,45 +140,17 @@ function createCanvas(img){
   memeCanvas.width = img.width;
   memeCanvas.height = img.height;
   ctx.drawImage(img,0 ,0 ,img.width, img.height);
-  $('#test').append(img);
+  setContextSettings();
   $('#saveMessage').text("Right click on the image to save it...");
 }
 
 
 function generateTopText(text){
-   var maxWidth = (memeCanvas.width-30);
-   var lineHeight = 40;
-      ctx.fillStyle = "white";
-      ctx.strokeStyle = "black";
-      ctx.font = "30pt Impact";
-      ctx.lineWidth = 2;
-      ctx.textAlign = "center";
-  
-      wrapTextTop(text,(memeCanvas.width)/2, 45, maxWidth, lineHeight);
-      
-      //wrapTextBottom(text[1],(memeCanvas.width)/2, (memeCanvas.height-15), maxWidth, lineHeight);  
-
-      
-      //ctx.fillText(text[1],(memeCanvas.width)/2, (memeCanvas.height-15));
-      //ctx.strokeText(text[1],(memeCanvas.width)/2, (memeCanvas.height-15));
+    wrapTextTop(text,(memeCanvas.width)/2, 45, maxWidth, lineHeight);
 }
 
 function generateBottomText(text){
-   var maxWidth = (memeCanvas.width-30);
-   var lineHeight = 40;
-      ctx.fillStyle = "white";
-      ctx.strokeStyle = "black";
-      ctx.font = "30pt Impact";
-      ctx.lineWidth = 2;
-      ctx.textAlign = "center";
-  
-      //wrapTextTop(text,(memeCanvas.width)/2, 45, maxWidth, lineHeight);
-      
-      wrapTextBottom(text,(memeCanvas.width)/2, (memeCanvas.height-15), maxWidth, lineHeight);  
-
-      
-      //ctx.fillText(text[1],(memeCanvas.width)/2, (memeCanvas.height-15));
-      //ctx.strokeText(text[1],(memeCanvas.width)/2, (memeCanvas.height-15));
+   wrapTextBottom(text,(memeCanvas.width)/2, (memeCanvas.height-15), maxWidth, lineHeight);  
 }
 
 
@@ -166,17 +159,13 @@ function wrapTextBottom(text, x, y, maxWidth, lineHeight) {
         var words = text.split(' ');
         var line = '';
         var linesArray = [];
-        //var y_old;
-
+ 
         for(var n = 0; n < words.length; n++) {
           var testLine = line + words[n] + ' ';
-          
-          console.log("---testLine: ",testLine);
+
           var metrics = ctx.measureText(testLine);
           var testWidth = metrics.width;
-          console.log("testWidth: ",testWidth," maxWidth: ",maxWidth);
-          console.log("x: ",x," y: ",y);
-          console.log("testWidth > maxWidth: ",(testWidth > maxWidth));
+
           if (testWidth > maxWidth) {
             
             linesArray.push(line);
@@ -187,13 +176,8 @@ function wrapTextBottom(text, x, y, maxWidth, lineHeight) {
               ctx.fillText(linesArray[i], x, (y-(len-(i))*lineHeight));
               ctx.strokeText(linesArray[i], x, (y-(len-(i))*lineHeight));
             }
-            //console.log("if block executed");
-            //y_old = y;
-            //y -= lineHeight;
-            //ctx.fillText(line, x, y);
-            //ctx.strokeText(line, x, y);
+
             line = words[n] + ' ';
-            //y = y_old;
           }
           else {
             line = testLine;
@@ -210,15 +194,11 @@ function wrapTextTop(text, x, y, maxWidth, lineHeight) {
 
         for(var n = 0; n < words.length; n++) {
           var testLine = line + words[n] + ' ';
-          
-          //console.log("Bottom testLine: ",testLine);
+
           var metrics = ctx.measureText(testLine);
           var testWidth = metrics.width;
-          //console.log("testWidth: ",testWidth," maxWidth: ",maxWidth);
-          //console.log("x: ",x," y: ",y);
-          //console.log("testWidth > maxWidth: ",(testWidth > maxWidth));
+ 
           if (testWidth > maxWidth) {
-            //console.log("if block executed");
             ctx.fillText(line, x, y);
             ctx.strokeText(line, x, y);
             line = words[n] + ' ';
